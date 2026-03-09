4d00870a92cb2b4e70326dcc1c9912b298a3aa69
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2026-03-09 16:41:24 +0200

    Uploaded the configuration file

---
+++ conf.py
@@ -0,0 +1,6 @@
+HOST = "127.0.0.1"
+LISTENING_PORT = 8007  # Port to listen for incoming messages from the server
+
+# Peer's listening port and sending port
+PEER_LISTENING_PORT = 8007  # Port to listen for incoming messages from the peer
+INTERFACE = "0.0.0.0"
